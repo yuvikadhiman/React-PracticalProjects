c2c3e929e3e98ae6bc7dc4e044b8d9da5b7e453a
yuvikadhiman <98342858+yuvikadhiman@users.noreply.github.com>
2023-06-18 23:47:52 +0530

    .

--- 08-navbar/src/Navbar.js
+++ 08-navbar/src/Navbar.js
@@ -7,14 +7,31 @@ const navbar = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [showLink,setShowLink]=useState(false)
     // console.log(links)
+   const Navlink=()=>{
+    return(
+        <div className="links-container">
+            <ul className="links">
+                {links.map((link)=>{
+                    const {url,id,text}=link;
+                    return(
+                        <li key={id}><a href={url}>{text}</a></li>
+                    )
+                    
+                })}
+            </ul>
+        </div>
+    )
+   }
 
   return (
     <nav>
       <div className="nav-center">
-        <div className="nav-header">3
+        <div className="nav-header">
             <img src={logo}  classNamealt="logo" alt='logo'/>
-            <button className='nav-toggle'><FaBars/></button>
+            <button className='nav-toggle' onClick={()=>setShowLink(!showLink)}><FaBars/></button>
         </div>
+        {showLink && <Navlink/>}
+        
 
       </div>
     </nav>
